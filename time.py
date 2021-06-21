a01ae2aef5c707a024fdbeeb4fab5460e9735972
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-06-21 07:42:15 +0700

    Update time.py

--- time.py
+++ time.py
@@ -1,12 +1,22 @@
 import time
+from datetime import date
+from datetime import datetime
 from threading import Timer
 
+today = date.today()
+now = datetime.now()
+d1 = today.strftime("%d/%m/%Y")
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
 def timeout_handler(timeout=10):
-    print time.time()
+    print("||||||||||||||||||||")
+    print("Today's date :", d1)
+    print("Now time :", dt_string)
+    print("||||||||||||||||||||")
     timer = Timer(timeout, timeout_handler)
     timer.start()
 
 timeout_handler()
 while True:
-    print "loop"
+    print("Loading...")
     time.sleep(1)
