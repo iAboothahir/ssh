4a0d6114e7f84e33bab11f7a85b2b08a2e325c93
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-06-21 07:52:00 +0700

    Update time.py

--- time.py
+++ time.py
@@ -1,22 +1,27 @@
+from random import seed
+from random import randint
 import time
 from datetime import date
 from datetime import datetime
 from threading import Timer
+
+seed(1)
+
+for _ in range(100):
+	value = randint(0, 100)
 
 today = date.today()
 now = datetime.now()
 d1 = today.strftime("%d/%m/%Y")
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
-def timeout_handler(timeout=10):
-    print("||||||||||||||||||||")
-    print("Today's date :", d1)
-    print("Now time :", dt_string)
-    print("||||||||||||||||||||")
+def timeout_handler(timeout=1):
+    print("Hôm nay là :", d1)
+    print("Bây giờ là :", dt_string)
     timer = Timer(timeout, timeout_handler)
     timer.start()
 
 timeout_handler()
 while True:
-    print("Loading...")
+    print("Loading...", value, "%")
     time.sleep(1)
